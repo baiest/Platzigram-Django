1e52acf880e18ce5a00b35a76f41dc110f213ab1
baiest <ballesteros.juan@correounivalle.edu.co>
2021-01-28 10:23:16 -0500

    Vista ejemplo de platzigram

--- posts/views.py
+++ posts/views.py
@@ -6,28 +6,28 @@ posts = [
         'title': 'Mont Blac',
         'user': {
             'name': 'Juan Ballesteros',
-            'picture': 'https://picsum.photos/200/200?image=958'
+            'picture': 'https://picsum.photos/60/60?image=958'
         },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200?image=1036'
+        'photo': 'https://picsum.photos/200/200?image=1036'
     },
     {
         'title': 'Via Lactea',
         'user': {
             'name': 'Camila Sanchez',
-            'picture': 'https://picsum.photos/200/200?image=958'
+            'picture': 'https://picsum.photos/60/60?image=958'
         },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200?image=415'
+        'photo': 'https://picsum.photos/200/200?image=415'
     },
     {
         'title': 'Auditorio',
         'user': {
             'name': 'Daniel Aristisaba',
-            'picture': 'https://picsum.photos/200/200?image=958'
+            'picture': 'https://picsum.photos/60/60?image=958'
         },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200?image=785'
+        'photo': 'https://picsum.photos/200/200?image=785'
     }
 ]
 
