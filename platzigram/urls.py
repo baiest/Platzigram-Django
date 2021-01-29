4765cf1202eb0678c5afbb55d6cc1f6b47afad67
baiest <ballesteros.juan@correounivalle.edu.co>
2021-01-29 17:05:12 -0500

    Agregado codigo para subir fotos a media

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path
+from django.conf.urls.static import static
 from platzigram import views as local_views
 from posts import views as post_views
 
@@ -25,5 +27,5 @@ urlpatterns = [
     path('hi/<str:name>/<int:age>', local_views.say_hi),
 
     path('posts/', post_views.list_posts)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
